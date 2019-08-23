a442fee77662f1600ba41e786d82016d4abc5c3d
Jesus <jesuscampos1975@gmail.com>
2019-08-23 22:20:56 +0200

    Injecting services

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes: Hero[] = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
